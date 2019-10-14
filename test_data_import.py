ba6f6e82ad437023338774261a61620d884ebce8
Wei-Tse Hsu <wehs7661@colorado.edu>
2019-10-14 11:29:07 -0600

    Developed test_roundtime

--- test_data_import.py
+++ test_data_import.py
@@ -4,15 +4,18 @@ import datetime
 import data_import
 
 class TestDataImport(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.csv_file = './smallData/meal_small.csv'
+        cls.obj = data_import.ImportData(cls.csv_file)
+
     def test_init_import(self):
         """
         A testing function for checking the data types and the length of the data
         """
-        csv_file = './smallData/meal_small.csv'
-        obj = data_import.ImportData(csv_file)
-        self.assertEqual(len(obj._time), len(obj._value))
-        self.assertEqual("<class 'int'>", str(type(obj._value[0])))
-        self.assertEqual("<class 'datetime.datetime'>", str(type(obj._time[0])))
+        self.assertEqual(len(self.obj._time), len(self.obj._value))
+        self.assertEqual("<class 'int'>", str(type(self.obj._value[0])))
+        self.assertEqual("<class 'datetime.datetime'>", str(type(self.obj._time[0])))
 
     def test_init_replace(self):
         """
@@ -21,22 +24,41 @@ class TestDataImport(unittest.TestCase):
         f = open('test.csv', 'w')
         f.write("Id,time,value\n1134,3/19/18 22:18,low")
         f.close()
-        obj = data_import.ImportData('test.csv', replace=True)
-        self.assertEqual(40, obj._value[0])
+        obj_init = data_import.ImportData('test.csv', replace=True)
+        self.assertEqual(40, obj_init._value[0])
         os.remove('test.csv')
 
     def test_linear_search(self):
         """
         A testing function for linear_search_value
         """
-        csv_file = './smallData/meal_small.csv'
-        obj = data_import.ImportData(csv_file)
-        time1 = datetime.datetime(2018, 3, 16, 0, 20, 0, 0)
-        time2 = datetime.datetime(2020, 3, 16, 0, 20, 0, 0)
-        val1 = obj.linear_search_value(time1)
-        val2 = obj.linear_search_value(time2)
-        self.assertEqual(val1, [0])
+        time1 = datetime.datetime(2018, 3, 16, 8, 42)
+        time2 = datetime.datetime(2020, 3, 12, 0, 0)
+        val1 = self.obj.linear_search_value(time1)
+        val2 = self.obj.linear_search_value(time2)
+        self.assertEqual(val1, [60])
         self.assertEqual(val2, -1)
+
+    def test_roundtime(self):
+        """
+        A testing function for rounTimeArray
+        """
+        obj_test1 = data_import.ImportData('./smallData/cgm_small.csv')
+        output = data_import.roundTimeArray(obj_test1, 30)
+        for (time, val) in output:
+            self.assertEqual(time.minute, 0)
+            self.assertEqual(val, (131 + 138 + 144)/3)
+            break
+
+        obj_test2 = data_import.ImportData('./smallData/activity_small.csv')
+        output = data_import.roundTimeArray(obj_test2, 40)
+        for (time, val) in output:
+            self.assertEqual(time.minute, 0)
+            self.assertEqual(val, (3 + 7 + 76 + 6 + 7))
+            break
+            
+
+
 
 
 if __name__ == '__main__':
